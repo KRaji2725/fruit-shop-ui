3dab9703880b2acb4d5475cc60bbb18c7b3654d5
Raji <therin576@gmail.com>
2026-06-04 14:01:35 +0530

    Fresh Mart level1 completed

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import { useNavigate } from "react-router-dom"
+
 function LogIn() {
   const navigate = useNavigate()
+  const [email,setEmail]=useState("")
+  const [password,setPassword]=useState("")
+  const [error,setError]=useState("")
+  const handleLogin=(e)=>{
+    e.preventDefault();
+    const savedUser=JSON.parse(localStorage.getItem("user"));
+    if(!savedUser){
+      setError("Please Sign Up First");
+      return
+    }
+    if(savedUser.email==email&&savedUser.password){
+      alert("login success fully")
+      navigate("/items")
+    }else{
+      setError("Invalid Email or Password")
+    }
+  }
   return (
     <div
       className="min-h-screen bg-cover bg-center flex justify-end items-center px-40"
@@ -15,26 +33,38 @@ function LogIn() {
         <h1 className="text-3xl font-bold text-center text-green-700 mb-6">
           Fresh Mart
         </h1>
-        <form className="space-y-4">
+        <form className="space-y-4" onSubmit={handleLogin}>
           <input
             type="email"
             placeholder="Email Address"
             className="w-full p-3 border rounded-lg outline-none focus:border-green-500"
+            onChange={(e)=>setEmail(e.target.value)}
           />
+          {
+            error&&(
+              <p className="text-red-500 text-center">{error}</p>
+            )
+          }
           <input
             type="password"
             placeholder="Password"
             className="w-full p-3 border rounded-lg outline-none focus:border-green-500"
+            onChange={(e)=>setPassword(e.target.value)}
           />
-
-        </form>
-        <button
+          {
+            error&&(
+              <p className="text-red-500 text-center">{error}</p>
+            )
+          }
+          <button
           type="submit"
-          className="w-full bg-green-600 text-white py-3 rounded-lg hover:bg-green-700 transition mt-4"
-          onClick={() => navigate('/items')}
+          className="w-full bg-green-600 text-white py-3 rounded-lg hover:bg-green-700 transition "
+         
         >
           Login
         </button>
+        </form>
+        
         <p className="text-center text-slate-900 mt-4">
           Don't have an account?{" "}
           <span className="text-blue-800 cursor-pointer hover:text-red-500 transition " onClick={() => navigate('/signup')}>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,56 +1,96 @@
-import React from 'react'
-import {useNavigate} from "react-router-dom"
+import React ,{useState} from 'react'
+import { useNavigate } from "react-router-dom"
+
 function SignUp() {
- const navigate=useNavigate()
-  return (
-   <div
-      className="min-h-screen bg-cover bg-center flex justify-end items-center px-40"
-      style={{
-        backgroundImage: "url('/images/login.png')",
-      }}
-    >
-      {/* Login Card */}
-      <div className="bg-white/10 backdrop-blur-md p-8 rounded-2xl shadow-xl w-[90%] max-w-md ">
+    const navigate = useNavigate()
+    const [name,setName]=useState("")
+    const [phoneNo,setPhoneNo]=useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+            
+       {/* signup handler */ }
+    const handleSingnup=(e)=>{
+        e.preventDefault();
+        if(password!==confirmPassword){
+         alert("Password do not match")
+         return;
+        }
+        const user={
+            name,
+            phoneNo,
+            email,
+            password,
+        };
+            localStorage.setItem("user",JSON.stringify(user));
+            alert("SigNUp Successfully");
+            navigate('/login');
+        }
+   
+    return (
+        <div
+            className="min-h-screen bg-cover bg-center flex justify-end items-center px-40"
+            style={{
+                backgroundImage: "url('/images/login.png')",
+            }}
+        >
+            {/* Login Card */}
 
-        <h1 className="text-3xl font-bold text-center text-green-700 mb-6">
-         Fresh Mart
-        </h1>
-        <form className="space-y-4">
-          <input
-            type="email"
-            placeholder="Email Address"
-            className="w-full p-3 border rounded-lg outline-none focus:border-green-500"
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            className="w-full p-3 border rounded-lg outline-none focus:border-green-500"
-          />
-          <input
-            type="confirm password"
-            placeholder="Confirm Password"
-            className="w-full p-3 border rounded-lg outline-none focus:border-green-500"
-          />
-          <button
-            type="submit"
-            className="w-full bg-green-600 text-white py-3 rounded-lg hover:bg-green-700 transition"
-            onClick={()=>navigate('/Login')}
-          >
-           sign up
-          </button>
-        </form>
+            <div className="bg-white/10 backdrop-blur-md p-8 rounded-2xl shadow-xl w-[90%] max-w-md ">
 
-        <p className="text-center text-slate-900 mt-4">
-         Already have an account?{" "}
-          <span className="text-blue-800 cursor-pointer hover:text-red-500 transition" onClick={()=>navigate('/login')}>
-            Log In
-          </span>
-        </p>
+                <h1 className="text-3xl font-bold text-center text-green-700 mb-6">
+                    Fresh Mart
+                </h1>
+                <form className="space-y-4" onSubmit={handleSingnup}>
+                     <input
+                        type="name"
+                        placeholder="Name"
+                        className="w-full p-3 border rounded-lg outline-none focus:border-green-500"
+                        onChange={(e)=>setName(e.target.value)}
+                    />
+                     <input
+                        type="phoneNo"
+                        placeholder="Phone NUmber"
+                        className="w-full p-3 border rounded-lg outline-none focus:border-green-500"
+                        onChange={(e)=>setPhoneNo(e.target.value)}
+                    />
+                    <input
+                        type="email"
+                        placeholder="Email Address"
+                        className="w-full p-3 border rounded-lg outline-none focus:border-green-500"
+                        onChange={(e)=>setEmail(e.target.value)}
+                    />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        className="w-full p-3 border rounded-lg outline-none focus:border-green-500"
+                        onChange={(e)=>setPassword(e.target.value)}
+                    />
+                    <input
+                        type="confirm password"
+                        placeholder="Confirm Password"
+                        className="w-full p-3 border rounded-lg outline-none focus:border-green-500"
+                        onChange={(e)=>setConfirmPassword(e.target.value)}
+                    />
+                    <button
+                        type="submit"
+                        className="w-full bg-green-600 text-white py-3 rounded-lg hover:bg-green-700 transition"
+                    >
+                        sign up
+                    </button>
+                </form>
 
-      </div>
-    </div>
+                <p className="text-center text-slate-900 mt-4">
+                    Already have an account?{" "}
+                    <span className="text-blue-800 cursor-pointer hover:text-red-500 transition" onClick={() => navigate('/login')}>
+                        Log In
+                    </span>
+                </p>
 
-  )
+            </div>
+        </div>
+
+    )
 }
 
 export default SignUp
